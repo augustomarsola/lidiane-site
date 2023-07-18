f38d7a59ce1754197a6032b5c5339ee505d68690
Augusto Marsola <augusto.marsola@live.com>
2023-07-17 21:55:03 -0300

    Adjusted menu container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { allTexts } from '@/server/cms/allTexts'
-import { MainBanner } from './_components/MainBanner'
-import { Services } from './_components/Services/Services'
-import { About } from './_components/About'
+import { MainBanner } from './_components/Home/MainBanner'
+import { Services } from './_components/Home/Services/Services'
+import { About } from './_components/Home/About'
 
 export default async function Home() {
   const { menuContent } = await allTexts()
